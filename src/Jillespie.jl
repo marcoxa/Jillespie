65abed73106abba4fd1c3ed4a5d400ba1fb000e3
Marco Antoniotti <marco.antoniotti@unimib.it>
2023-11-05 22:18:12 +0100

    Checkpoint version.

--- src/Jillespie.jl
+++ src/Jillespie.jl
@@ -1,10 +1,10 @@
 ### -*- Mode: Julia -*-
 
-"
+"""
 module Jillespie
 
 An implementation of Gillespie's Stochastic/Montecarlo simulation algorithm.
-"
+"""
 module Jillespie
 
 using StatsBase
@@ -13,7 +13,12 @@ using Distributions
 
 export greet, Model, simulate
 
-"The Jillespie module greeting."
+
+"""
+greet
+
+The Jillespie module greeting.
+"""
 greet() = print("Jillespie: a Gillespie simulation algorithm  implementation.")
 
 macro lambda(args, body)
@@ -24,41 +29,46 @@ end
 ### Model
 ### =====
 
-"
+"""
 Model
 
 A collection of species and reactions.
-"
+"""
 struct Model
+    reactions
     inital_state
     state
     state_changes
-    reactions
-    
-    function Model(initial_state :: Dict{Symbol, Int64}, reactions)
-        new(initial_state,
+    t0
+
+    ## Inner constructor.
+    function Model(reactions, initial_state :: Dict{Symbol, Int64})
+        new(reactions,
+            initial_state,
             copy(initial_state),
             [],
-            reactions)
+            0.0
+            )
     end
 end
 
 
-function Model(species :: Vector{Symbol}, reacts)
-    Model(Dict{Symbol, Int64}(map(Pair,
+function Model(reacts, species :: Vector{Symbol})
+    Model(reacts,
+          Dict{Symbol, Int64}(map(Pair,
                                   species,
                                   zeros(Int64, length(species)))),
-          reacts)
+          )
 end
 
 
 function Model(initial :: Vector{Tuple{Symbol, Int64}}, reacts)
-    Model(Dict(intial), reacts)
+    Model(reacts, Dict(intial))
 end
 
 
 function Model(initial)
-    Model(initial, [])
+    Model([], initial)
 end
 
 
@@ -67,14 +77,17 @@ end
 
 function add_specie(model, symbol, initial = 0)
     @assert initial >= 0
-    model.species[symbol] = initial
+    if ! (v in initial_state)  # v must be in model.state
+        model.initial_state[symbol] = initial
+        model.state[symbol] = initial
+    end
 end
 
 
 function set_specie(model, symbol, n)
     @assert n >= 0
-    @assert symbol in keys(model.species)
-    model.species[symbol] = n
+    @assert symbol in keys(model.state)
+    model.state[symbol] = n
 end
 
 
@@ -143,9 +156,11 @@ function current_propensity(reaction)
     m =  reaction.model
     vs = reaction.reactants
     ## s =  reaction.stoichiometry
+    current_state = m.state
     p =  reaction.propensity
+          
 
-    p(map(v -> m.state[v], vs))
+    p(map(v -> current_state[v], vs))
 end
 
 
@@ -156,36 +171,37 @@ end
     simulate(model; start, finish)
 
 Simulate a model from `start` to `end`, intended as time.
+The function returns a vector of 'times' corresponding to each
+reaction occurence, the corresponding vector of `state_changes` from
+one state to the next and the model `initial_state`.
+The actual state trace at each time step can be reconstructed
+afterward (we are not in the 80's anymore, but it is still nice to
+save memory).
 """
 function simulate(model; start = 0, finish = 1024)
-    local times = [0.0]
-    local counts = model.initials
-    local state = []
-    local rates = []
-    local reactions = model.reactions
+    times = [0.0]
+    rates = []
+    reactions = model.reactions
 
     while times[end] <= finish
-        state = counts[end]
 
-        rates = [r.propensity(selectvars(r, state)... for r in reactions] 
+        rates = map(current_propensity, reactions)
 
         if all(rate == 0 for rate in rates)
             break
         end
 
-        transition = rand(reactions)
-        next_state = advance_state(transition, state)
+        transition = sample(reactions, rates)
+        happen(trasition)
 
         dt = rand(Exponential(sum(rates)))
 
         push!(times, times[end] + dt)
-        append!(counts, next_state)
     end
 
-    return times, counts
+    return times, model.state_changes, model.initial_state
 end
 
 end # module Jillespie
 
 ### end of file Jillespie.jl
-
